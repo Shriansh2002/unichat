5d4aa4f284088ab60edef92744a861a336edb45e
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-03-28 20:31:36 +0530

    Updates

---
+++ precache-manifest.2edf8516f09e625e1666be301518439c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0cbfed770b16ab5027653577e9a21e",
+    "url": "/unichat/index.html"
+  },
+  {
+    "revision": "35912105ea6ea6b75ab6",
+    "url": "/unichat/static/css/main.edebb4a2.chunk.css"
+  },
+  {
+    "revision": "55120735709d0ebcda86",
+    "url": "/unichat/static/js/2.8953a72e.chunk.js"
+  },
+  {
+    "revision": "563cec0a8f134cb3c9b5211a1992f1ed",
+    "url": "/unichat/static/js/2.8953a72e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35912105ea6ea6b75ab6",
+    "url": "/unichat/static/js/main.dbc3e70b.chunk.js"
+  },
+  {
+    "revision": "dbe73014955cc44a12db",
+    "url": "/unichat/static/js/runtime-main.de7dfe23.js"
+  }
+]);
